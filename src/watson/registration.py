6ab130a547f0f3bc4e16c06a31e649c04a0d46ea
Dave Hall <dave@etianen.com>
2011-08-21 15:15:15 +0100

    Added in tests for registration.

--- src/watson/registration.py
+++ src/watson/registration.py
@@ -5,6 +5,21 @@ class SearchAdaptor(object):
 
     """An adaptor for performing a full-text search on a model."""
     
+    # If set to True, then the search engine will only return
+    # models accessible from the default manager of the model. This allows
+    # you to limit the results according to some custom publication of your
+    # own, at the expense of some search performance.
+    live_filter = False
+    
+    # Use to specify the fields that should be included in the seach.
+    fields = None
+    
+    # Use to exclude fields from the search.
+    exclude = None
+    
+    # Use to specify object properties to be stored in the search index.
+    store = None
+    
     def __init__(self, model):
         """Initializes the search adaptor."""
         self.model = model
@@ -17,16 +32,42 @@ class SearchAdaptor(object):
         # Add in the URL.
         if hasattr(obj, "get_absolute_url"):
             meta["url"] = obj.get_absolute_url()
+        # Add in the stored fields.    
+        if self.store:
+            for field_name in self.store:
+                value = getattr(obj, field_name)
+                if callable(value):
+                    value = value()
+                meta[field_name] = value
         # Return the meta information.
         return meta
         
     def get_search_text(self, obj):
         """Returns the search text associated with the given obj."""
-        return unicode(obj)
+        # Get the field names to look up.
+        if self.fields is None:
+            field_names = [field.name for field in self.model._meta.fields if isinstance(field, (models.CharField, models.TextField))]
+        else:
+            field_names = self.fields
+        # Exclude named fields.
+        if exclude:
+            field_names = [field_name for field_name in field_names if field_name not in self.exclude]
+        # Create the text.
+        text_parts = []
+        for field_name in field_names:
+            # Resolve the value.
+            value = getattr(obj, field_name)
+            if callable(value):
+                value = value()
+            value = unicode(value)
+            # Store the value.
+            text_parts.append(value)
+        # Consolidate the text.
+        return u"".join(text_parts)
         
     def get_weighted_search_text(self, obj):
         """Returns the weighted search text associated with the given obj."""
-        return (self.get_search_text(),)
+        return (unicode(obj), self.get_search_text(),)
 
 
 class RegistrationError(Exception):
@@ -44,14 +85,9 @@ def is_registered(model):
     return model in _registered_models
 
 
-def register(model, live_filter=False):
+def register(model, adaptor_cls=SearchAdaptor):
     """
     Registers the given model with django-watson.
-    
-    If live_filter is set to True, then the search engine will only return
-    models accessible from the default manager of the model. This allows
-    you to limit the results according to some custom publication of your
-    own, at the expense of some search performance.
     
     If the given model is already registered with django-watson, a
     RegistrationError will be raised.
@@ -62,13 +98,9 @@ def register(model, live_filter=False):
         raise RegistrationError("{model!r} is already registered with django-watson".format(
             model = model,
         ))
-    # Resolve the live queryset.
-    if live_filter:
-        live_queryset = model._default_manager.all()
-    else:
-        live_queryset = None
     # Perform the registration.
-    _registered_models[model] = live_queryset
+    adaptor_obj = adaptor_cls(model)
+    _registered_models[model] = adaptor_obj
     
     
 def unregister(model):
@@ -87,7 +119,17 @@ def unregister(model):
         ))
         
         
-def get_registered_models(model):
+def get_registered_models():
     """Returns a sequence of models that have been registered with django-watson."""
     global _registered_models
     return _registered_models.keys()
+    
+    
+def get_adaptor(model):
+    """Returns the adaptor associated with the given model."""
+    global _registered_models
+    if is_registered(model):
+        return _registered_models[model]
+    raise RegistrationError("{model!r} not registered with django-watson".format(
+        model = model,
+    ))
--- src/watson/tests.py
+++ src/watson/tests.py
@@ -1,3 +1,29 @@
 """Tests for django-watson."""
 
+from django.db import models
 from django.test import TestCase
+
+from watson.registration import register, unregister, is_registered, get_registered_models, get_adaptor, RegistrationError, SearchAdaptor
+
+
+class TestModel(models.Model):
+
+    class Meta:
+        app_label = "auth"  # Hack: Cannot use an app_label that is under South control, due to http://south.aeracode.org/ticket/520
+
+
+class RegistrationText(TestCase):
+    
+    def testRegistration(self):
+        # Register the model and test.
+        register(TestModel)
+        self.assertTrue(is_registered(TestModel))
+        self.assertRaises(RegistrationError, lambda: register(TestModel))
+        self.assertEqual(get_registered_models(), [TestModel])
+        self.assertTrue(isinstance(get_adaptor(TestModel), SearchAdaptor))
+        # Unregister the model and text.
+        unregister(TestModel)
+        self.assertFalse(is_registered(TestModel))
+        self.assertRaises(RegistrationError, lambda: unregister(TestModel))
+        self.assertEqual(get_registered_models(), [])
+        self.assertRaises(RegistrationError, lambda: isinstance(get_adaptor(TestModel)))
