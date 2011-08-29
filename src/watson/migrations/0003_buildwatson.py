2a58bab8f8642ecc8b5d454f57dcea0809722e2f
Dave Hall <dave@etianen.com>
2011-08-29 16:17:47 +0100

    Adding buildwatson command to the migration installation script.

---
+++ src/watson/migrations/0003_buildwatson.py
@@ -0,0 +1,44 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+from django.core.management import call_command
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        "Write your forwards methods here."
+        
+        # Needs to be run in a separate migration to avoid borking MySQL.
+        call_command("buildwatson", verbosity=0)
+
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+
+    models = {
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'watson.searchentry': {
+            'Meta': {'object_name': 'SearchEntry'},
+            'content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'engine_slug': ('django.db.models.fields.CharField', [], {'max_length': '200', 'db_index': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'meta_encoded': ('django.db.models.fields.TextField', [], {}),
+            'object_id': ('django.db.models.fields.TextField', [], {}),
+            'object_id_int': ('django.db.models.fields.IntegerField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '1000', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['watson']
