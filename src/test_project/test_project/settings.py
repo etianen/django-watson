b982b607d319270a0bb8415c0ee6714f1ee773ff
Dave Hall <dave@etianen.com>
2013-12-03 19:16:59 +0000

    Another attempt to fix test project MySQL collation settings

--- src/test_project/test_project/settings.py
+++ src/test_project/test_project/settings.py
@@ -65,7 +65,9 @@ DATABASES = {
     }
 }
 if DATABASES["default"]["ENGINE"] == "django.db.backends.mysql":
-    DATABASES["default"]["OPTIONS"] = "SET storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_general_ci"
+    DATABASES["default"]["OPTIONS"] = {
+        "init_command": "SET storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_general_ci",
+    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
