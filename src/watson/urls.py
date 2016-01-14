552f09f7ebbb8a96e217a91ecd8c10c6035964aa
SimonGreenhill <simon@simon.net.nz>
2016-01-14 14:02:12 +1300

    fixing DeprecationWarning

--- src/watson/urls.py
+++ src/watson/urls.py
@@ -4,11 +4,12 @@ from __future__ import unicode_literals
 
 from django.conf.urls import url, patterns
 
+from watson.views import search, search_json
 
-urlpatterns = patterns("watson.views",
+urlpatterns = [
 
-    url("^$", "search", name="search"),
+    url("^$", search, name="search"),
     
-    url("^json/$", "search_json", name="search_json"),
+    url("^json/$", search_json, name="search_json"),
 
-)
+]
