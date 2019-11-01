585dbf7d825fada0872163ce45b74abe58a77b1e
Dave Hall <dave@etianen.com>
2019-11-01 10:48:27 +0000

    Adding some test matrix exclusions

--- setup.py
+++ setup.py
@@ -33,9 +33,10 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         "Framework :: Django",
     ],
 )
